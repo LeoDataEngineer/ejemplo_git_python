24040559cb206e0ee1ac8cdbf79edd2bb028b063
Leomar Estrada <leomarestradabsas@gmail.com>
2024-03-06 18:41:36 -0300

    actalizar las funciones pompy

--- ejemplo_git.py
+++ ejemplo_git.py
@@ -17,3 +17,21 @@ def resta (a,b):
     return f"La sumu es: {a - b}"
 
 
+
+print("La funcion divide 2 numeros enteros:")
+
+def divide(a,b):
+    
+    return f"La sumu es: {a / b}"
+
+
+print(divide(2,3))
+
+print("La funcion resta 2 numeros enteros:")
+
+def multiplicacion(a,b):
+    
+    return f"La sumu es: {a * b}"
+
+
+
